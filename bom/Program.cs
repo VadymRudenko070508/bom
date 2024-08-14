05001e408e9d7655ced25b04470ef765a317bddc
VadymRudenko070508 <vadimrudenko070508@gmail.com>
2024-08-14 12:39:42 +0300

    Добавьте файлы проекта.

---
+++ bom/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    
+    static int[] GenerateRandomArray(int size, int minValue, int maxValue)
+    {
+        int[] array = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(minValue, maxValue);
+        }
+
+        return array;
+    }
+
+    
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.WriteLine(element);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int[] randomArray = GenerateRandomArray(10, 0, 100);
+
+        
+        PrintArray(randomArray);
+    }
+}
